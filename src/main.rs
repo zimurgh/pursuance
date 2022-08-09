9f88ed3b2f6cebbfe0c0c396998cb34f2d577800
Michael Carpenter <mcarpenter.dev@gmail.com>
2022-08-09 09:39:11 -0400

    Add db conn

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use pursuance::run;
+use pursuance::configuration::get_configuration;
+use pursuance::startup::run;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
