7088a87cc06c6be4ce2636ddf024f18f59760769
Michael Carpenter <mcarpenter.dev@gmail.com>
2022-08-10 17:13:27 -0400

    Add telemetry

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
-use env_logger::Env;
 use pursuance::configuration::get_configuration;
 use pursuance::startup::run;
-use sqlx::PgPool;
+use pursuance::telemetry::{get_subscriber, init_subscriber};
+use secrecy::ExposeSecret;
+use sqlx::postgres::PgPool;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let subscriber = get_subscriber("pursuance".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres");
+    let connection_pool =
+        PgPool::connect(&configuration.database.connection_string().expose_secret())
+            .await
+            .expect("Failed to connect to Postgres");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await
