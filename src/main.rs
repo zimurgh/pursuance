81fac135dba31f940efa0c120be9262832976e27
Michael Carpenter <mcarpenter.dev@gmail.com>
2022-08-09 20:00:25 -0400

    Fix CI

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use pursuance::configuration::get_configuration;
 use pursuance::startup::run;
+use sqlx::PgPool;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
