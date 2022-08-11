0abf1f97efab009f795e24e0c0817aed0eef6c6f
Michael Carpenter <mcarpenter.dev@gmail.com>
2022-08-11 18:04:09 -0400

    Update db configs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use pursuance::configuration::get_configuration;
 use pursuance::startup::run;
 use pursuance::telemetry::{get_subscriber, init_subscriber};
-use secrecy::ExposeSecret;
-use sqlx::postgres::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 
 #[tokio::main]
@@ -11,9 +10,9 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool =
-        PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
-            .expect("Failed to connect to Postgres");
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy_with(configuration.database.with_db());
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
